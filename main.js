38ef61e345a178bb3a9719cc83f5b51eaff203c0
yuval isaac <yuval828@walla.com>
2019-12-07 16:39:23 +0200

    local search

--- main.js
+++ main.js
@@ -2,17 +2,41 @@
 "use strict";
 
 $(() => {
-
+    let allCoinsArray = [];
     getAjaxData("https://api.coingecko.com/api/v3/coins/list", response => displayCoins(response));
 
 
     //טרם טופל חיפוש
     $("#searchCoins").click(() => {
-        let searchCountryVal = $("input").val();
-        searchCountryVal == "" ?
-            alert("please enter a Symbol name: like BTC or Name: like bitcoin") :
-            getAjaxData(`https://api.coingecko.com/api/v3/coins/${searchCountryVal}`, response => displayCoin(response));
+        let searchCoin = $("input").val();
+        if (searchCoin == "") {
+            alert("please enter a Symbol name: like BTC or Name: like bitcoin");
+        } else {
+            $.grep(allCoinsArray, function(obj) {
+                if (obj.symbol === searchCoin) {
+                    displayCoin(obj);
+                }
+                return;
+            })[0];
+            $.grep(allCoinsArray, function(obj) {
+                if (obj.name === searchCoin) {
+                    displayCoin(obj);
+                }
+                return;
+            })[0];
+        }
+        // } else {
+        //     alert(`you didnt enter a correct coin please enter a Symbol name: like BTC or Name: like bitcoin`);
+
+        // }
     });
+
+
+    function displayCoin(Coin) {
+        let str = drawCoin(Coin);
+        $("#allCoins").empty();
+        $("#allCoins").append(str);
+    }
 
     $(".getInfo").click(() => {
         getAjaxData(`https://api.coingecko.com/api/v3/coins/${item.id}`, response => displayMoreInfo(response));
@@ -31,31 +55,27 @@ $(() => {
     }
 
 
-    function displayCoin(Coin) {
-        let obj = drawCoin(Coin);
-        $("#allCoins").empty();
-        $("#allCoins").append(obj);
-    }
+
 
     function displayCoins(Coins) {
         $("#allCoins").empty();
         // for (let index = 0; index < 10; index++) {
-        const arr = new Array;
+        allCoinsArray = Coins.slice(0, 10);
         let i = 0;
         for (const item of Coins) {
             i++;
             if (i <= 10) {
-                let obj = drawCoin(item);
+                let str = drawCoin(item);
 
-                $("#allCoins").append(obj);
-                var coinsArr = arr.push(item);
+                $("#allCoins").append(str);
+                // var coinsArr = arr.push(item);
 
-            } else return coinsArr;
+            } //else return coinsArr;
         }
     }
 
     function displayMoreInfo(id) {
-        const obj = `
+        const str = `
          <div class="collapse" id="collapse${item.symbol}">
             <div class="card-body">
             <h5 class="card-title">${id.data_market.current_price.usd}</h5>
@@ -67,12 +87,12 @@ $(() => {
             </div>
         </div>
                             `
-        $("#allCoins").append(obj);
+        $("#allCoins").append(str);
     }
 
 
     function drawCoin(item) {
-        const obj = `
+        const str = `
                     <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12" >
                     <div class="card-body">
                     <label class="switch">
@@ -94,7 +114,7 @@ $(() => {
                     </div>
                     </div>
                     `;
-        return obj;
+        return str;
     }
 
     // $("p>button").click(function() {
