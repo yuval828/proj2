38b8c0d0ad8ea08c514deba6127744348020808f
yuval isaac <yuval828@walla.com>
2019-12-07 11:14:11 +0200

    why return dont do this?

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 "use strict";
 
 $(() => {
-    $("#getAllCoins").click(() => {
-        getAjaxData("https://api.coingecko.com/api/v3/coins/list", response => displayCoins(response));
-    });
+
+    getAjaxData("https://api.coingecko.com/api/v3/coins/list", response => displayCoins(response));
+
 
     //טרם טופל חיפוש
     $("#searchCoins").click(() => {
-        let searchCountryVal = $("input:text").val();
+        let searchCountryVal = $("input").val();
         searchCountryVal == "" ?
-            alert("please enter a country name") :
-            getAjaxData(`https://restcountries.eu/rest/v2/name/${searchCountryVal}?fields=name;topLevelDomain;capital;currencies;flag`, response => displayCoins(response));
+            alert("please enter a Symbol name: like BTC or Name: like bitcoin") :
+            getAjaxData(`https://api.coingecko.com/api/v3/coins/${searchCountryVal}`, response => displayCoin(response));
     });
 
     $(".getInfo").click(() => {
@@ -31,15 +31,47 @@ $(() => {
     }
 
 
+    function displayCoin(Coin) {
+        drewCoin(Coin);
+        $("#allCoins").append(obj);
+    }
+
     function displayCoins(Coins) {
         $("#allCoins").empty();
         // for (let index = 0; index < 10; index++) {
+        const arr = new Array;
         let i = 0;
         for (const item of Coins) {
             i++;
             if (i <= 10) {
+                drewCoin(item);
 
-                const obj = `
+                $("#allCoins").append(obj);
+                var coinsArr = arr.push(item);
+
+            } else return coinsArr;
+        }
+    }
+
+    function displayMoreInfo(id) {
+        const obj = `
+         <div class="collapse" id="collapse${item.symbol}">
+            <div class="card card-body">
+            <h5 class="card-title">${id.data_market.current_price.usd}</h5>
+            <h5 class="card-title">${id.data_market.current_price.euro}</h5>
+            <h5 class="card-title">${id.data_market.current_price.nis}</h5>
+              <span>
+                  <img class="imgCountry" src="${id.image.small}" >
+              </span>                  
+            </div>
+        </div>
+                            `
+        $("#allCoins").append(obj);
+    }
+
+
+    function drewCoin(item) {
+        const obj = `
                     <div class="card col-xl-4 col-lg-4 col-md-4 col-sm-12" >
                     <div class="card-body">
                     <label class="switch">
@@ -61,32 +93,8 @@ $(() => {
                     </div>
                     </div>
                     `;
-                $("#allCoins").append(obj);
-
-            } else return;
-
-
-        }
+        return obj;
     }
-
-    function displayMoreInfo(id) {
-        const obj = `
-         <div class="collapse" id="collapse${item.symbol}">
-            <div class="card card-body">
-            <h5 class="card-title">${id.data_market.current_price.usd}</h5>
-            <h5 class="card-title">${id.data_market.current_price.euro}</h5>
-            <h5 class="card-title">${id.data_market.current_price.nis}</h5>
-              <span>
-                  <img class="imgCountry" src="${id.image.small}" >
-              </span>                  
-            </div>
-        </div>
-                            `
-        $("#allCoins").append(obj);
-    }
-
-
-
 
     // $("p>button").click(function() {
     //     alert(this.innerText);
