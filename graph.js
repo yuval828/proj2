4eef80383290455b7405be17ef8e541bc3e01ec9
yuval isaac <yuval828@walla.com>
2019-12-23 23:55:45 +0200

    all working ex graph des

--- graph.js
+++ graph.js
@@ -6,7 +6,6 @@ $(() => {
     let interval;
     let chart;
 
-    // let tableArr = switchedArray;
     /*LiveReports graph*/
     $("#LiveReports").click(() => {
         if (interval) {
@@ -17,29 +16,29 @@ $(() => {
             chart.destroy()
             chart = null
         }
-        const symbols = switchedArray.map(t => t.symbol).join(",");
-        // let priceCoinArr = [];
-        // let dataPoints = [];
-        // let dataPoints1 = [];
-        // let dataPoints2 = [];
-        // let dataPoints3 = [];
-        // let dataPoints4 = [];
+        const symbols = switchedArray.map(t => t.symbol).join(","); //get all the symbols of coins we choosed
 
         $("#allCoins").empty();
         $(`#headerSpinner`).addClass("loader");
         $(".bgimg-1").css({ "background-image": `url("img/LiveReports.jpg")`, "min-height": "50%" });
         $("#firstParalex").text(`
-        come see the graph....
+            Scroll down to see your graph
         `);
         $(".bgimg-2").css({ "background-image": `url("img/LiveReports.jpg")`, "min-height": "50%" });
-        $("#secondParalex").text(`
-        your graph....
-        `);
+        $("#secondParalex").text(``);
         $(`#headerSpinner`).removeClass("loader");
         $(`footer`).html(`
-        <div id="chartContainer" style="height: 100%; width: 100%;"></div>
+            <div id="chartContainer" style="height: 100%; width: 100%;"></div>
         `);
-        updateData();
+
+        updateData(); //get first time data
+
+        function updateData() {
+            getAjaxData(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${symbols}&tsyms=USD`, response =>
+                drawGraph(response),
+            );
+        }
+
 
         function getAjaxData(url, callback) {
             $.ajax({
@@ -55,81 +54,107 @@ $(() => {
         function drawGraph(response) {
             if (response.Response === "Error") {
                 $("#secondParalex").text(`
-                The Coins you choosed didnt returned any value
+                    The Coins you choosed didnt return any value
                 `);
                 return
-            }
-
-            const now = new Date()
-            for (const coin in response) {
-                //coin = ETH
-                const price = response[coin].USD;
-                let dataItem = data.find(t => t.name == coin)
-                if (!dataItem) {
-                    dataItem = {
-                        type: "spline",
-                        showInLegend: true,
-                        name: coin,
-                        xValueFormatString: "mm:ss",
-                        yValueFormatString: "$#,##0.#####",
-                        dataPoints: [],
-                    };
-                    data.push(dataItem);
+            } else {
+                const now = new Date()
+                for (const coin in response) {
+                    //coin = ETH
+                    const price = response[coin].USD;
+                    let dataItem = data.find(t => t.name == coin)
+                    if (!dataItem) {
+                        dataItem = {
+                            type: "spline",
+                            showInLegend: true,
+                            name: coin,
+                            xValueFormatString: "mm:ss",
+                            yValueFormatString: "$#,##0.#####",
+                            dataPoints: [],
+                        };
+                        data.push(dataItem);
+                    }
+                    dataItem.dataPoints.push({ x: now, y: price })
                 }
-                dataItem.dataPoints.push({ x: now, y: price })
-            }
 
-            var options = {
-                animationEnabled: true,
-                title: {
-                    text: `${symbols} to USD`,
-                },
-                axisX: {
-                    title: "time",
-                    valueFormatString: "mm:ss",
-                },
-                axisY: {
-                    title: "price in usd",
-                    titleFontColor: "#4F81BC",
-                    lineColor: "#4F81BC",
-                    labelFontColor: "#4F81BC",
-                    tickColor: "#4F81BC",
-                    includeZero: false,
-                },
-                toolTip: {
-                    shared: true,
-                },
-                legend: {
-                    cursor: "pointer",
-                    itemclick: toggleDataSeries,
-                },
-                data: data,
-            };
+                var options = {
+                    animationEnabled: true,
+                    title: {
+                        text: `${symbols} to USD`,
+                    },
+                    axisX: {
+                        title: "time",
+                        valueFormatString: "mm:ss",
+                    },
+                    axisY: {
+                        title: "price in usd",
+                        titleFontColor: "#4F81BC",
+                        lineColor: "#4F81BC",
+                        labelFontColor: "#4F81BC",
+                        tickColor: "#4F81BC",
+                        includeZero: false,
+                    },
+                    toolTip: {
+                        shared: true,
+                    },
+                    legend: {
+                        cursor: "pointer",
+                        itemclick: toggleDataSeries,
+                    },
+                    data: data,
+                };
 
-            if (!chart) {
-                chart = new CanvasJS.Chart("chartContainer", options); //$("#chartContainer").CanvasJSChart(options);
-            }
-            chart.render()
-
-            function toggleDataSeries(e) {
-                if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
-                    e.dataSeries.visible = false;
-                } else {
-                    e.dataSeries.visible = true;
+                if (!chart) {
+                    chart = new CanvasJS.Chart("chartContainer", options); //$("#chartContainer").CanvasJSChart(options);
                 }
-                e.chart.render();
+                chart.render()
+
+                function toggleDataSeries(e) {
+                    if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
+                        e.dataSeries.visible = false;
+                    } else {
+                        e.dataSeries.visible = true;
+                    }
+                    e.chart.render();
+                }
+
+                // update chart every second
+                interval = setInterval(function() {
+                    updateData();
+                }, 2000);
             }
         }
+    });
 
-        // update chart every second
-        interval = setInterval(function() {
-            updateData();
-        }, 2000);
-
-        function updateData() {
-            getAjaxData(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${symbols}&tsyms=USD`, response =>
-                drawGraph(response),
-            );
+    //go to about if about press
+    $("#About").click(() => {
+        if (interval) {
+            clearInterval(interval)
+            interval = null
         }
+        if (chart) {
+            chart.destroy()
+            chart = null
+        }
+        $("#allCoins").empty();
+        $("#chartContainer").empty();
+        $(`footer`).empty();
+        $(`#headerSpinner`).addClass("loader");
+        $(".bgimg-1").css({ "background-image": `url("img/aboutBobi.jpg")`, "min-height": "100%" });
+        $("#firstParalex").html(`
+            
+            ברוכים הבאים, שמי יובל יצחק <br>
+            בודק תוכנה בחברת השכר מיכפל<br>
+            בן 37, נשוי לטל <br>
+            גר בגבעתיים <br>
+            ומאוד אוהב את הכלבים שלי בובי וליבי
+        `);
+        $(".bgimg-2").css({ "background-image": `url("img/aboutLibi.jpg")`, "min-height": "100%" });
+        $("#secondParalex").html(`
+            Full Stack Web Developer <br>
+            jQuery-AJAX API Project <br>
+        `);
+        $(`#headerSpinner`).removeClass("loader");
+
     });
 });
--- main.js
+++ main.js
@@ -15,19 +15,20 @@ $(() => {
     getAjaxData("https://api.coingecko.com/api/v3/coins/list", response => displayAllCoins(response));
 
 
-    //get all coins at home press
+    //see all coins at home press
     $("#Home").click(() => {
         HomePageDisplay();
         displayAllCoins(allCoinsArray);
     });
 
+    //the homepage we will see at refresh, pressing home or pressing X at search or search
     function HomePageDisplay() {
         $(`footer`).html(`<div id="allCoins" class="row bgimg-3"></div>`);
         $(`#headerSpinner`).addClass("loader");
         $(".bgimg-1").css({ "background-image": `url("img/homeHeader.jpg")`, "min-height": "50%" });
-        $("#firstParalex").html("<b><u>crypTrack</b></u><br> the crypto database");
+        $("#firstParalex").html("<b><u>crypTrack</b></u><br> The Crypto Database");
         $(".bgimg-2").css({ "background-image": `url("img/homeHeader.jpg")`, "min-height": "50%" });
-        $("#secondParalex").text("choose your coins to see at graph");
+        $("#secondParalex").text("");
         $(`#headerSpinner`).removeClass("loader");
     }
 
@@ -42,68 +43,56 @@ $(() => {
     }
 
 
-    /** display all coins */
-    function displayAllCoins(Coins) { //run on the array for how many coins we want to draw
+    /* draw all coins */
+    function displayAllCoins(Coins) {
         $(`footer`).html(`<div id="allCoins" class="row bgimg-3"></div>`);
         $("#allCoins").empty();
         $(`#headerSpinner`).addClass("loader");
-        allCoinsArray = Coins.slice(500, 1000); //slice the array for how much coins we want to see
-        let i = 0;
-        for (const item of allCoinsArray) { //can run on allCoinsArray and dont need if and index
-            i++;
-            if (i <= 500) {
-                let el = drawOneCoin(item);
-                $("#allCoins").append(el);
-                $(`#headerSpinner`).removeClass("loader");
-            }
+        allCoinsArray = Coins.slice(0, 1000); //slice the array for how many coins we want to see
+        for (const item of allCoinsArray) { //run on allCoinsArray
+            let el = drawOneCoin(item); //draw one coin
+            $("#allCoins").append(el);
+            $(`#headerSpinner`).removeClass("loader");
         }
     }
-    /** display all coins end */
 
-    // switchEl.find("input").on("change", e => console.log(e.currentTarget.checked))
 
-    /* a single coin draw*/
+    /* a single coin draw */
     function drawOneCoin(item) {
         const str = `
         <div class="coinDiv col-xl-3 col-lg-3 col-md-4 col-sm-12 " >
-            <div class=" row">
+            <div class="row">
                 <h5 class="card-title col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">${item.symbol}</h5>
-                <label class="switch" >
+                <label class="switch">
                     <input type="checkbox" id="switch${item.symbol}" ${item.isChecked ? "checked":""}>
                     <span class="slider round"></span>
                 </label>
             </div>
             <hr>
-                <p class="card-text">${item.name}</p>
-                <p>
-                <button class="btn btn-primary getInfo " id="${item.id}" data-toggle="collapse" data-target="#collapse${item.id}" aria-expanded="false" aria-controls="collapse">
+            <p class="card-text">${item.name}</p>  
+            <button class="btn btn-primary getInfo" id="${item.id}" data-toggle="collapse" data-target="#collapse${item.id}" aria-expanded="false" aria-controls="collapse">
                 <span id="showSpinner${item.id}"></span>
-                        More Info
-                    </button>
-                <div class="collapse" id="collapse${item.id}">
-                   
-                </div>
-            
+                More Info
+            </button>
+            <div class="collapse" id="collapse${item.id}"></div>            
         </div>
         `;
-        const el = $(str)[0]; //get all the div
-        // const checkbox = $(el).find(".switch > input")[0]
+        const el = $(str)[0]; //get an element from the string
 
 
-        /*chack if switch pressed */
+        /* chack if switch pressed on coin */
         $(el).find(".switch > input").click(function(e) {
             const checkbox = e.target;
             const isChecked = checkbox.checked;
 
-            if (switchedArray.length == 0) { //אם מערך 0 תכניס את השם למערך
+            if (switchedArray.length == 0) { //if switchedArray is empty go in, marked as checked and push the coin info 
                 item.isChecked = checkbox.checked;
                 switchedArray.push(item);
                 return;
-            } else { // אם מערך לא ריק 
-                if (switchedArray.length <= 5) {
-
-                    for (let index = 0; index < switchedArray.length; index++) { // כל עוד אינדקס פחות מאורך המערך
-                        if (switchedArray[index].symbol === item.symbol) { //בודק האם השם נמצא במערך אם כן מוחק אותו
+            } else {
+                if (switchedArray.length <= 5) { //if switchedArray is smaller or equel to 5 go in and check if is on the array
+                    for (let index = 0; index < switchedArray.length; index++) {
+                        if (switchedArray[index].symbol === item.symbol) { //if in the array remove the checked and delete it
                             item.isChecked = false;
                             switchedArray.splice(index, 1);
                             return;
@@ -112,20 +101,48 @@ $(() => {
                     item.isChecked = checkbox.checked;
                     switchedArray.push(item);
 
-                    if (switchedArray.length == 6) {
+                    if (switchedArray.length == 6) { //if we pressed on the 6 coin, marked as unchecked and remove from switchedArray
                         item.isChecked = false;
                         sixCoin = item;
                         switchedArray.splice(switchedArray.length - 1, 1)
-
-
-                        Modal(sixCoin);
+                        Modal(sixCoin); //open the Modal with the sixcoin we pressed
                     }
                 }
             }
         });
 
 
+        /* draw the modal with six coin (the last one is unchecked) */
+        function Modal(sixCoin) {
+            let str = `
+            <div class="modal fade" id="myModal" role="dialog">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">The maximum coins you are allowed to choose is 5. please uncheck one and choose another or press close to exit </h4>
+                        </div>
+                        <div class="coins"></div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+            const headerModal = $(str)[0];
+            const coinsToDisplay = [...switchedArray, sixCoin] //add the two array to "coinsToDisplay"
+            for (const item of coinsToDisplay) { //draw all six coins at modal
+                const coinEl = drawOneCoinModal(item);
+                $(headerModal).find(".coins").append(coinEl)
+            }
 
+            $(headerModal).modal().on("hide.bs.modal", e => { //when exiting modal display all coins 
+                displayAllCoins(allCoinsArray)
+            })
+        }
+
+
+        /*draw one coin at the modal */
         function drawOneCoinModal(item) {
             let str =
                 `<div class="row">
@@ -135,84 +152,49 @@ $(() => {
                     <span class="slider round"></span>
                 </label>
                 </div>`
-            const el = $(str)[0]; //get all the div
+            const el = $(str)[0];
+
+            /*chack if switch pressed on modal*/
             $(el).find(".switch > input").click(function(e) {
                 const checkbox = e.target;
                 const isChecked = checkbox.checked;
 
-                if (switchedArray.find(el => el.symbol == item.symbol)) {
+                if (switchedArray.find(el => el.symbol == item.symbol)) { //see if the coin that was pressed is allready on the switchedArray
                     item.isChecked = false;
-                    switchedArray = switchedArray.filter(e => e !== item)
-                    if (sixCoin.enterToArr) {
+                    switchedArray = switchedArray.filter(e => e !== item) //if is in remove it
+                    if (sixCoin.enterToArr) { //and if enterToArr(sixCoin) is true go in make it false, mark it as checked and push to array
                         sixCoin.enterToArr = false;
                         sixCoin.isChecked = true;
                         switchedArray.push(sixCoin);
                     }
                     return;
                 } else {
-                    if (switchedArray.length < 5) {
+                    if (switchedArray.length < 5) { //if switchedArray is less then 5,checked the coin push to the array
                         item.isChecked = checkbox.checked;
                         switchedArray.push(item);
-                    } else {
+                    } else { // if the first coin we press is the sixCoin - marked enterToArr as true
                         sixCoin = item;
                         sixCoin.enterToArr = checkbox.checked;
                         return sixCoin;
                     }
                 }
-
-
-
             })
             return el;
         }
 
 
-
-        // מצייר את המודל עם 6 המטבעות שבחרנו השישי לא מסומן כנבחר
-        function Modal(sixCoin) {
-
-            let str = `<div class="modal fade" id="myModal" role="dialog">
-              <div class="modal-dialog">
-              
-                <!-- Modal content-->
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">the maximum coins you are allowed is five, please unchecked one and save. or press close to remove the last coin</h4>
-                  </div>
-                  <div class="coins"></div>
-                  <div class="modal-footer">
-         
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                  </div>
-                </div>`
-            const headerModal = $(str)[0];
-            const coinsToDisplay = [...switchedArray, sixCoin] //add the two array to "coinsToDisplay"
-            for (const item of coinsToDisplay) {
-                const coinEl = drawOneCoinModal(item);
-                $(headerModal).find(".coins").append(coinEl)
-            }
-
-            $(headerModal).modal().on("hide.bs.modal", e => {
-                displayAllCoins(allCoinsArray)
-            })
-        }
-
-
-
-
         /*chack if more info pressed */
         $(".getInfo", el).click(function(e) {
             let obj1 = e.currentTarget;
-            if (obj1.attributes[4].value == "false") { //only if MoreInfo is closed!
-                $(`#showSpinner${item.id}`).addClass('spinner-border spinner-border-sm');
+            if (obj1.attributes[4].value == "false") { //only if MoreInfo is closed go in
+                $(`#showSpinner${item.id}`).addClass('spinner-border spinner-border-sm'); //show spinner at the button
                 let islocal = false;
                 let market_cap_rank = 0;
                 $.grep(MoreInfoArray, function(obj) {
                     if (obj.id === obj1.id) { //if obj is on the arr
-                        if (obj.endTime > Date.parse(new Date)) { //if now time is smaller then time in arr+2min
+                        if (obj.endTime > Date.parse(new Date)) { //if now time is smaller then time in arr + 2min
                             islocal = true;
-                            displayMoreInfo(obj, islocal); //enter for local
+                            displayMoreInfo(obj, islocal); //enter for local storage
                             return;
                         } else {
                             islocal = "15";
@@ -222,7 +204,7 @@ $(() => {
                 })[0];
 
 
-                if (islocal == "15") {
+                if (islocal == "15") { //chack if the coin info is on the local array if so remove it and get new data
                     for (let index = 0; index < MoreInfoArray.length; index++) {
                         if (MoreInfoArray[index].market_cap_rank === Number(market_cap_rank)) {
                             MoreInfoArray.splice(index, 1);
@@ -232,15 +214,16 @@ $(() => {
                     }
                 }
 
-                if (!islocal) {
+                if (!islocal) { //if its not marked as local get info
                     getAjaxData(`https://api.coingecko.com/api/v3/coins/${(obj1.id)}`, response => displayMoreInfo(response, islocal));
                 }
             } else {
-                $(`#${obj1.id}`).text("More Info");
+                $(`#${obj1.id}`).text("More Info"); //if more info is showing close the info and change the button name
             }
         });
         return el
     }
+
 
     /*get More Info for coin - and then store it for 2 min for islocal usage*/
     function displayMoreInfo(obj, islocal) {
@@ -250,10 +233,8 @@ $(() => {
         }
 
         $(`#collapse${obj.id}`).empty()
-        $(`#${obj.id}`).text("Less info");; // לדיב אם רוצים כרקע style="background-image: url(${obj.image.large}); background-repeat: no-repeat";
-        // $(`#collapse${obj.id}`).addClass("loader");
+        $(`#${obj.id}`).text("Less info");; // when open change button to less info
         const str = `          
-            </div>
             <div class="card-body">
                 <h5><u>Current Price:</u></h5>
                 <p class="card-info">Dollar: ${obj.market_data.current_price.usd}&dollar;</p>
@@ -263,7 +244,7 @@ $(() => {
                   <img class="imgCountry" src="${obj.image.large}" >
                 </span>                  
             </div>`;
-        // $(`#collapse${obj.id}`).removeClass("loader");
+
         $(`#collapse${obj.id}`).append(str);
         $(`#showSpinner${obj.id}`).removeClass('spinner-border spinner-border-sm');
     }
@@ -271,7 +252,7 @@ $(() => {
 
 
     /*search a coin*/
-    //search if enter pressed 
+    // if enter pressed 
     $("input").keypress(event => {
         if (event.keyCode === 13) {
             event.preventDefault();
@@ -279,18 +260,16 @@ $(() => {
         }
     });
 
-    // if x-clear is pressed go to Home! 
+    // if x-clear is pressed go to Home 
     $("input").bind("mouseup", function(e) {
         var $input = $(this),
             oldValue = $input.val();
-
         if (oldValue == "") return;
 
         // When this event is fired after clicking on the clear button
         // the value is not cleared yet. We have to wait for it.
         setTimeout(function() {
             var newValue = $input.val();
-
             if (newValue == "") {
                 // capture the clear
                 $input.trigger("cleared");
@@ -301,17 +280,17 @@ $(() => {
 
     //search if search button pressed 
     $("#searchCoins").click(() => {
-
         $(`#headerSpinner`).addClass("loader");
         let searchCoin = $("input").val();
 
-        searchCoin = searchCoin.toLowerCase();
+        searchCoin = searchCoin.toLowerCase(); //will search without case sensitive
         let coinAtList = false;
-        if (searchCoin == "") {
+        if (searchCoin == "") { //if search is empty
             $(`#headerSpinner`).removeClass("loader");
-            alert("please enter a Symbol name like: ZOC \n or Name like: 01coin");
+            $("#secondParalex").text("please enter a Symbol name like: btc");
             return;
         }
+
         $.grep(allCoinsArray, function(obj) {
             if (obj.symbol === searchCoin) {
                 displaySearchCoin(obj);
@@ -321,18 +300,12 @@ $(() => {
                 return;
             }
         })[0];
-        $.grep(allCoinsArray, function(obj) {
-            if (obj.name === searchCoin) {
-                displaySearchCoin(obj);
-                $("#firstParalex").text("Found your Coin name");
-                $("#secondParalex").text("scroll down");
-                coinAtList = true;
-                return;
-            }
-        })[0];
+
         if (!coinAtList) {
             $(`#headerSpinner`).removeClass("loader");
-            alert(`the coin you entered is not on the list \n please enter a correct coin Symbol name like: ZOC \n or Name like: 01coin`);
+            alert(`
+            The coin you entered is not on the list \n
+            please enter a correct Coin Symbol name like: btc`);
         }
     });
 
@@ -340,38 +313,14 @@ $(() => {
     function displaySearchCoin(Coin) {
         HomePageDisplay();
         $(`footer`).html(`
-        <div id="allCoins" class="row bgimg-3"></div>
+            <div id="allCoins" class="row bgimg-3"></div>
         `);
         $("#allCoins").empty();
-        // $("#chartContainer").empty().attr("height", "0");
 
-        // $(`#spinner`).addClass("loader");
         let el = drawOneCoin(Coin);
         $(`#headerSpinner`).removeClass("loader");
-        // $(`#spinner`).removeClass("loader");
         $("#allCoins").append(el);
     }
     /*search a coin end*/
 
-
-    //go to about if about press
-    $("#About").click(() => {
-        $("#allCoins").empty();
-        $("#chartContainer").empty();
-        $(`#headerSpinner`).addClass("loader");
-        $(".bgimg-1").css({ "background-image": `url("img/aboutBobi.jpg")`, "min-height": "100%" });
-        $("#firstParalex").html(`welcome to my site yuval isaac....<br>
-        infoinfo infoinfoinfo <br> infoinfo infoinfoinfo
-       `);
-        $(".bgimg-2").css({ "background-image": `url("img/aboutLibi.jpg")`, "min-height": "100%" });
-        $("#secondParalex").html(`מה נעשה בפרוייקט...<br>
-        infoinfo infoinfo infoinfo infoinfo <br>
-        infoinfo infoinfo <br> infoinfo infoinfo`);
-        $(`#headerSpinner`).removeClass("loader");
-        // $("#allCoins").html(
-        //     ` <div>
-        //         about me...
-        //         </div>`
-        // );
-    });
 });
