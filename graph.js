bfc8edcaa5e6209a4449fb3288d8f23f4cb5cf17
yuval isaac <yuval828@walla.com>
2019-12-24 00:05:55 +0200

    s

--- graph.js
+++ graph.js
@@ -30,8 +30,15 @@ $(() => {
         $(`footer`).html(`
             <div id="chartContainer" style="height: 100%; width: 100%;"></div>
         `);
+        const data = [];
 
         updateData(); //get first time data
+
+        // update chart every second
+        interval = setInterval(function() {
+            updateData();
+        }, 2000);
+
 
         function updateData() {
             getAjaxData(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${symbols}&tsyms=USD`, response =>
@@ -49,7 +56,6 @@ $(() => {
             });
         }
 
-        const data = [];
 
         function drawGraph(response) {
             if (response.Response === "Error") {
@@ -57,85 +63,78 @@ $(() => {
                     The Coins you choosed didnt return any value
                 `);
                 return
-            } else {
-                const now = new Date()
-                for (const coin in response) {
-                    //coin = ETH
-                    const price = response[coin].USD;
-                    let dataItem = data.find(t => t.name == coin)
-                    if (!dataItem) {
-                        dataItem = {
-                            type: "spline",
-                            showInLegend: true,
-                            name: coin,
-                            xValueFormatString: "mm:ss",
-                            yValueFormatString: "$#,##0.#####",
-                            dataPoints: [],
-                        };
-                        data.push(dataItem);
-                    }
-                    dataItem.dataPoints.push({ x: now, y: price })
+            }
+            const now = new Date()
+            for (const coin in response) {
+                const price = response[coin].USD;
+                let dataItem = data.find(t => t.name == coin)
+                if (!dataItem) {
+                    dataItem = {
+                        type: "spline",
+                        showInLegend: true,
+                        name: coin,
+                        xValueFormatString: "mm:ss",
+                        yValueFormatString: "$#,##0.#####",
+                        dataPoints: [],
+                    };
+                    data.push(dataItem);
                 }
+                dataItem.dataPoints.push({ x: now, y: price })
+            }
 
-                var options = {
-                    animationEnabled: true,
-                    title: {
-                        text: `${symbols} to USD`,
-                    },
-                    axisX: {
-                        title: "time",
-                        valueFormatString: "mm:ss",
-                    },
-                    axisY: {
-                        title: "price in usd",
-                        titleFontColor: "#4F81BC",
-                        lineColor: "#4F81BC",
-                        labelFontColor: "#4F81BC",
-                        tickColor: "#4F81BC",
-                        includeZero: false,
-                    },
-                    toolTip: {
-                        shared: true,
-                    },
-                    legend: {
-                        cursor: "pointer",
-                        itemclick: toggleDataSeries,
-                    },
-                    data: data,
-                };
+            const options = {
+                animationEnabled: true,
+                title: {
+                    text: `${symbols} to USD`,
+                },
+                axisX: {
+                    title: "time",
+                    valueFormatString: "mm:ss",
+                },
+                axisY: {
+                    title: "price in usd",
+                    titleFontColor: "#4F81BC",
+                    lineColor: "#4F81BC",
+                    labelFontColor: "#4F81BC",
+                    tickColor: "#4F81BC",
+                    includeZero: false,
+                },
+                toolTip: {
+                    shared: true,
+                },
+                legend: {
+                    cursor: "pointer",
+                    itemclick: toggleDataSeries,
+                },
+                data: data,
+            };
 
-                if (!chart) {
-                    chart = new CanvasJS.Chart("chartContainer", options); //$("#chartContainer").CanvasJSChart(options);
+            if (!chart) {
+                chart = new CanvasJS.Chart("chartContainer", options);
+            }
+            chart.render()
+
+            function toggleDataSeries(e) {
+                if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                } else {
+                    e.dataSeries.visible = true;
                 }
-                chart.render()
-
-                function toggleDataSeries(e) {
-                    if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
-                        e.dataSeries.visible = false;
-                    } else {
-                        e.dataSeries.visible = true;
-                    }
-                    e.chart.render();
-                }
-
-                // update chart every second
-                interval = setInterval(function() {
-                    updateData();
-                }, 2000);
+                e.chart.render();
             }
         }
     });
 
     //go to about if about press
     $("#About").click(() => {
-        if (interval) {
-            clearInterval(interval)
-            interval = null
-        }
-        if (chart) {
-            chart.destroy()
-            chart = null
-        }
+        // if (interval) {
+        //     clearInterval(interval)
+        //     interval = null
+        // }
+        // if (chart) {
+        //     chart.destroy()
+        //     chart = null
+        // }
         $("#allCoins").empty();
         $("#chartContainer").empty();
         $(`footer`).empty();
